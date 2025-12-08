6cee14bdf6c8631a9460d590d83f54eb17931f1b
haotq2003 <khongnhoten2003@gmail.com>
2025-12-08 21:12:12 +0700

    login

--- navigation/AppNavigator.jsx
+++ navigation/AppNavigator.jsx
@@ -33,6 +33,7 @@ import DesignerHomeScreen2 from "../screens/designer/DesignerHomeScreen/Designer
 import DesignerProductsScreen from "../screens/designer/DesignerProductsScreen/DesignerProductsScreen";
 import DesignerAnalyticsScreen from "../screens/designer/DesignerAnalyticsScreen/DesignerAnalyticsScreen";
 import DesignerQuickUploadScreen from "../screens/designer/DesignerQuickUploadScreen/DesignerQuickUploadScreen";
+import CreateVersionScreen from "../screens/designer/CreateVersionScreen/CreateVersionScreen";
 import DesignerSubscription from "../screens/store/DesignerSubscription/DesignerSubscription";
 import DesignerWalletScreen from "../screens/designer/DesignerWalletScreen/DesignerWalletScreen";
 import ProfileScreen from "../screens/auth/ProfileScreen/ProfileScreen";
@@ -117,6 +118,10 @@ export default function AppNavigator() {
       <Stack.Screen
         name="DesignerQuickUpload"
         component={DesignerQuickUploadScreen}
+      />
+      <Stack.Screen
+        name="CreateVersionScreen"
+        component={CreateVersionScreen}
       />
 
 
